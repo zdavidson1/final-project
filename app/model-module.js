5005b45fd51b1a6a8119d087085cadf0fd435821
Zachary Davidson <zddavids@iu.edu>
2023-03-05 20:49:49 -0500

    Initial commit

---
+++ app/model-module.js
@@ -0,0 +1,321 @@
+var _homeContent = ` <div class="homepage-hero">
+<h1 class="homepage-text">Header goes here</h1>
+<h3 class="homepage-text2">Less important text goes here</h3>
+<div class="homepage-text3">
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+</div>
+<button class="read-more">Read More</button>
+</div>
+<section class="homepage-quote">
+<p class="quote">
+  “Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab
+  illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+  explicabo. Iste natus error sit voluptatem accusantium doloremque
+  laudantium totam"
+</p>
+<h5 class="john-smith">John Smith</h5>
+<p class="john-job">Corporation CEO, books author.</p>
+</section>
+
+<section class="event-container">
+<h1 class="event-title2">Events:</h1>
+<div class="date-container">
+  <div class="event-date-box">06 <br />JUN</div>
+  <h3 class="event-title">Sed eet persipiatis</h3>
+  <h3 class="event-description">
+    Lorem ipsum dolor sit emt, consecteur adipisincing elit.
+  </h3>
+  <div class="event-date-box">06 <br />JUN</div>
+  <h3 class="event-title">Sed eet persipiatis</h3>
+  <h3 class="event-description">
+    Lorem ipsum dolor sit emt, consecteur adipisincing elit.
+  </h3>
+  <div class="event-date-box">06 <br />JUN</div>
+  <h3 class="event-title">Sed eet persipiatis</h3>
+  <h3 class="event-description">
+    Lorem ipsum dolor sit emt, consecteur adipisincing elit.
+  </h3>
+  <div class="event-date-box">06 <br />JUN</div>
+  <h3 class="event-title">Sed eet persipiatis</h3>
+  <h3 class="event-description">
+    Lorem ipsum dolor sit emt, consecteur adipisincing elit.
+  </h3>
+</div>
+</section>
+<section class="footer-container">
+<img src="./images/logo/logo-white.png" alt="" class="footer-logo" />
+<h3>
+  420 Smoke St. Los Angeles, CA 46318 <br />
+  (213)-467-2627 <br />
+  info@eventsy.com
+</h3>
+<input
+  type="email"
+  name="email"
+  class="search-bar"
+  placeholder="Enter your email to join newsletter..."
+/>
+<button class="subscribe">SUBSCRIBE</button>
+<div class="footer-links">
+  <a href="www.facebook.com">
+    <img src="./images/social-media/facebook.svg" alt="facebook" />
+  </a>
+  <a href="www.twitter.com">
+    <img src="./images/social-media/instagram.svg" alt="twitter" />
+  </a>
+  <a href="www.instagram.com">
+    <img src="./images/social-media/twitter.svg" alt="instagram" />
+  </a>
+</div>
+<h4>Copyright 2019 eventsy. All rights reserved</h4>
+</section>`;
+var _aboutContent = `  <div class="about-hero">
+<div class="color-overlay6"></div>
+</div>
+<h1 class="about-title">OUR HISTORY:</h1>
+<h3 class="about-text">
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque
+obcaecati non quaerat neque, asperiores libero, debitis delectus sed
+numquam in pariatur excepturi. Pariatur accusamus nesciunt qui incidunt
+asperiores consequuntur iusto!
+</h3>
+<section class="footer-container">
+<img src="./images/logo/logo-white.png" alt="" class="footer-logo" />
+<h3>
+  420 Smoke St. Los Angeles, CA 46318 <br />
+  (213)-467-2627 <br />
+  info@eventsy.com
+</h3>
+<input
+  type="email"
+  name="email"
+  class="search-bar"
+  placeholder="Enter your email to join newsletter..."
+/>
+<button class="subscribe">SUBSCRIBE</button>
+<div class="footer-links">
+  <a href="www.facebook.com">
+    <img src="./images/social-media/facebook.svg" alt="facebook" />
+  </a>
+  <a href="www.twitter.com">
+    <img src="./images/social-media/instagram.svg" alt="twitter" />
+  </a>
+  <a href="www.instagram.com">
+    <img src="./images/social-media/twitter.svg" alt="instagram" />
+  </a>
+</div>
+<h4>Copyright 2019 eventsy. All rights reserved</h4>
+</section>`;
+var _galleryContent = `<h1 class="gallery-title">GALLERY:</h1>
+<div class="gallery-row1">
+  <a href="picture1.html"
+    ><div class="img1">
+      <div class="color-overlay1"></div></div
+  ></a>
+  <a href="picture2.html"
+    ><div class="img2">
+      <div class="color-overlay2"></div></div
+  ></a>
+  <a href="picture3.html"
+    ><div class="img3">
+      <div class="color-overlay3"></div></div
+  ></a>
+</div>
+<div class="gallery-row2">
+  <a href="picture4.html"
+    ><div class="img4">
+      <div class="color-overlay4"></div></div
+  ></a>
+  <a href="picture5.html"
+    ><div class="img5">
+      <div class="color-overlay5"></div></div
+  ></a>
+</div>
+<section class="footer-container">
+  <img src="./images/logo/logo-white.png" alt="" class="footer-logo" />
+  <h3>
+    420 Smoke St. Los Angeles, CA 46318 <br />
+    (213)-467-2627 <br />
+    info@eventsy.com
+  </h3>
+  <input
+    type="email"
+    name="email"
+    class="search-bar"
+    placeholder="Enter your email to join newsletter..."
+  />
+  <button class="subscribe">SUBSCRIBE</button>
+  <div class="footer-links">
+    <a href="www.facebook.com">
+      <img src="./images/social-media/facebook.svg" alt="facebook" />
+    </a>
+    <a href="www.twitter.com">
+      <img src="./images/social-media/instagram.svg" alt="twitter" />
+    </a>
+    <a href="www.instagram.com">
+      <img src="./images/social-media/twitter.svg" alt="instagram" />
+    </a>
+  </div>
+  <h4>Copyright 2019 eventsy. All rights reserved</h4>
+</section>`;
+var _blogContent = `<h1 class="blog-title">BLOG:</h1>
+<div class="blog-first-row">
+  <div class="blog-entry1">
+    <div class="Date-box">
+      <span>06</span>
+      <span>JUN</span>
+    </div>
+    <a href="blogpost.html"><div class="blog-img"></div></a>
+    
+  </div>
+  <div class="blog-entry1">
+    <div class="Date-box2">
+      <span>06</span>
+      <span>JUN</span>
+    </div>
+    <a href="blogpost1.html"><div class="blog-img"></div></a>
+  </div>
+</div>
+<div class="blog-text-container">
+  <h1 class="blog-entry-title">
+    Lorem et persipiatis unde <br />
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+    asperiores a nemo magni. Id, nostrum necessitatibus, repudiandae
+    quasi, vero sed harum consequuntur corporis quod tempora rerum
+    repellendus omnis ipsum consequatur.
+  </h1>
+  <h1 class="blog-entry-title">
+    Lorem et persipiatis unde <br />
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+    asperiores a nemo magni. Id, nostrum necessitatibus, repudiandae
+    quasi, vero sed harum consequuntur corporis quod tempora rerum
+    repellendus omnis ipsum consequatur.
+  </h1>
+</div>
+
+<div class="blog-first-row">
+  <div class="blog-entry1">
+    <div class="Date-box">
+      <span>06</span>
+      <span>JUN</span>
+    </div>
+    <a href="blogpost.html"><div class="blog-img"></div></a>
+  </div>
+  <div class="blog-entry1">
+    <div class="Date-box2">
+      <span>06</span>
+      <span>JUN</span>
+    </div>
+    <a href="blogpost.html"><div class="blog-img"></div></a>
+  </div>
+</div>
+<div class="blog-text-container">
+  <h1 class="blog-entry-title">
+    Lorem et persipiatis unde <br />
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+    asperiores a nemo magni. Id, nostrum necessitatibus, repudiandae
+    quasi, vero sed harum consequuntur corporis quod tempora rerum
+    repellendus omnis ipsum consequatur.
+  </h1>
+  <h1 class="blog-entry-title">
+    Lorem et persipiatis unde <br />
+    <br />
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+    asperiores a nemo magni. Id, nostrum necessitatibus, repudiandae
+    quasi, vero sed harum consequuntur corporis quod tempora rerum
+    repellendus omnis ipsum consequatur.
+  </h1>
+</div>
+<section class="footer-container">
+  <img src="./images/logo/logo-white.png" alt="" class="footer-logo" />
+  <h3>
+    420 Smoke St. Los Angeles, CA 46318 <br />
+    (213)-467-2627 <br />
+    info@eventsy.com
+  </h3>
+  <input
+    type="email"
+    name="email"
+    class="search-bar"
+    placeholder="Enter your email to join newsletter..."
+  />
+  <button class="subscribe">SUBSCRIBE</button>
+  <div class="footer-links">
+    <a href="www.facebook.com">
+      <img src="./images/social-media/facebook.svg" alt="facebook" />
+    </a>
+    <a href="www.twitter.com">
+      <img src="./images/social-media/instagram.svg" alt="twitter" />
+    </a>
+    <a href="www.instagram.com">
+      <img src="./images/social-media/twitter.svg" alt="instagram" />
+    </a>
+  </div>
+  <h4>Copyright 2019 eventsy. All rights reserved</h4>
+</section>`;
+var _contactContent = `  <h1 class="contact-title">CONTACT US:</h1>
+      <div class="contact-wrapper">
+        <div class="contact-form">
+          <div class="form-row">
+            <input type="text" placeholder="Your name..." />
+          </div>
+          <div class="form-row">
+            <input type="text" placeholder="Email Address..." />
+          </div>
+          <div class="form-row">
+            <input type="text" placeholder="Company..." />
+          </div>
+          <div class="form-row">
+            <input
+              style="height: 199px; vertical-align: top"
+              type="text"
+              placeholder="Message"
+            />
+          </div>
+          <button class="send-message-btn">SEND MESSAGE</button>
+        </div>
+        <div class="contact-img"></div>
+      </div>
+      <section class="footer-container">
+        <img src="./images/logo/logo-white.png" alt="" class="footer-logo" />
+        <h3>
+          420 Smoke St. Los Angeles, CA 46318 <br />
+          (213)-467-2627 <br />
+          info@eventsy.com
+        </h3>
+        <input
+          type="email"
+          name="email"
+          class="search-bar"
+          placeholder="Enter your email to join newsletter..."
+        />
+        <button class="subscribe">SUBSCRIBE</button>
+        <div class="footer-links">
+          <a href="www.facebook.com">
+            <img src="./images/social-media/facebook.svg" alt="facebook" />
+          </a>
+          <a href="www.twitter.com">
+            <img src="./images/social-media/instagram.svg" alt="twitter" />
+          </a>
+          <a href="www.instagram.com">
+            <img src="./images/social-media/twitter.svg" alt="instagram" />
+          </a>
+        </div>
+        <h4>Copyright 2019 eventsy. All rights reserved</h4>
+      </section>`;
+var _foodContent = `<h1>h1</h1>`;
+
+var _foodContent = `<h1>hi</h1>`;
+var _concertContent = `<h1>hi</h1>`;
+
+export var fn = "Zach";
+
+export function updateView(pageName) {
+  console.log("Model " + pageName);
+  var pageContent = "_" + pageName;
+  $("#app").html(eval(pageContent));
+}
